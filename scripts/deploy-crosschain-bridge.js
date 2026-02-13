879fd634bf679544d539ac6ba550be60d544ab16
Archi Baldezh <archibaldezh@gmail.com>
2026-02-13 15:00:51 +0200

    Update deploy-crosschain-bridge.js

--- scripts/deploy-crosschain-bridge.js
+++ scripts/deploy-crosschain-bridge.js
@@ -8,7 +8,7 @@ async function main() {
   console.log("Deploying contracts with the account:", deployer.address);
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  // Деплой контракта
+
   const CrossChainBridge = await ethers.getContractFactory("CrossChainBridgeV3");
   const bridge = await CrossChainBridge.deploy(
     250, // 2.5% fee percentage
